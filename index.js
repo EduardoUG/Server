cb41d66975e2bb26dadaed7754de6305081349fe
Eduardo Uribe <eduardo.uribe@handcloud.com.mx>
2017-04-03 22:06:20 -0500

    Modificación del index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const express = require('express'),
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.post('/login', (req,res) => {
+app.post('/login', function (req,res) {
 	let body = req.body
 	res.status(200).send({datos: body})
 })
 
-app.post('/register', (req,res) => {
+app.post('/register', function (req,res) {
 	let body = req.body
 	res.status(200).send({ message: 'You are registered'})
 })
